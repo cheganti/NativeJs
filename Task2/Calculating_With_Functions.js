9dd9267a0bc92d66c881df0081693310a07387bd
Just RaviC <cheganti@gmail.com>
2019-01-04 14:44:21 +0530

    Create Calculating_With_Functions.js

---
+++ Task2/Calculating_With_Functions.js
@@ -0,0 +1,16 @@
+ function zero() {
+            return recur(0, op);
+        }
+        function one() { return 1; }
+        function two() { return 2; }
+        function three() { return 3; }
+        function four(fr) { return fr + 4; }
+        function five() { return 5; }
+        function six(si) { return 6 / si; }
+        function seven(b) { return b * 7; }
+        function eight(ei) { return 8 - ei; }
+        function nine() { return 9; }
+        function plus(p) { return p; }
+        function minus(mi) { return mi; }
+        function times(a) { return a; }
+        function dividedBy(di) { return di; }
